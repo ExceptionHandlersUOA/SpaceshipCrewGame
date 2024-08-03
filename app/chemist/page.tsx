20fd8b501aeb610e72b6fa70511e7b73e93b4bb4
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-08-03 18:27:10 +1200

    Create elementTube tsx and have it and 2 buttons in chemist page.

--- app/chemist/page.tsx
+++ app/chemist/page.tsx
@@ -1,26 +1,37 @@
+'use client'
+
+import { ElementTube } from '../ui/chemist/elementTube';
 import './page.css';
 
 export default function Page() {
+    const handleButtonClick = (element: string) => {
+        console.log(element);
+    };
+
     return (
-      <main className="main">
-        <div className="description">
-          <p>
-            Get started by editing&nbsp;
-            <code className="code">app/page.tsx</code>
-          </p>
-          <div>
-            <p>I'm a thing in the top right</p>
-          </div>
-        </div>
-  
-        <div className="center">
-            hello
-        </div>
-  
-        <div className="grid">
-            classname grid
-        </div>
-      </main>
+        <main className="main">
+            <div className="description">
+                <p>
+                    Get started by editing&nbsp;
+                    <code className="code">app/page.tsx</code>
+                </p>
+                <div>
+                    <p>I'm a thing in the top right</p>
+                </div>
+            </div>
+
+            <div className="center">
+                <ElementTube />
+                <div>
+                    <button onClick={() => handleButtonClick("H")}>Hydrogen</button>
+                    <button onClick={() => handleButtonClick("O")}>Oxygen</button>
+                </div>
+
+            </div>
+
+            <div className="grid">
+                classname grid
+            </div>
+        </main>
     );
-  }
-  
+}
---
+++ app/ui/chemist/elementTube.tsx
@@ -0,0 +1,34 @@
+export function ElementTube() {
+    return (
+        <p>Hello</p>
+    );
+}
+
+//function Square({ value, onSquareClick }: { value: String, onSquareClick: () => void }) {
+//     return (
+//         <button className="square" onClick={onSquareClick}>
+//             {value}
+//         </button>
+//     );
+// }
+
+// function Board({ xIsNext, squares }: { xIsNext: Boolean, squares: }) {
+//     function handleClick(i) {
+//         const nextSquares = squares.slice();
+//         if (xIsNext) {
+//             nextSquares[i] = 'X';
+//         }
+//         else {
+//             nextSquares[i] = 'O';
+//         }
+//         onPlay(nextSquares);
+//     }
+
+//     return (
+//         <>
+//             <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+//             <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+//             <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+//         </>
+//     );
+// }
