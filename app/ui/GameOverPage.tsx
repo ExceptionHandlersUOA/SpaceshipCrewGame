9a3077b05b9cc367818ccd383af50011e44837bf
Henry Ly <160989916+henryhlly@users.noreply.github.com>
2024-08-04 11:55:13 +1200

    Implemented gameover page without styling has way of changing appearance

---
+++ app/ui/GameOverPage.tsx
@@ -0,0 +1,9 @@
+import styles from './GameOverPage.module.css'
+
+export default function GameOverPage() {
+    return (
+        <div className={styles.gameOverPage}>
+            Hello
+        </div>
+    );
+}
