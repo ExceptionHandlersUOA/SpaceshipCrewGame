ab37e7e4fb8e76e55d38662e2b3eaa70c8d7c8ef
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-08-03 17:30:41 +1200

    Create chemist page and css.

---
+++ app/chemist/page.tsx
@@ -0,0 +1,26 @@
+import './page.css';
+
+export default function Page() {
+    return (
+      <main className="main">
+        <div className="description">
+          <p>
+            Get started by editing&nbsp;
+            <code className="code">app/page.tsx</code>
+          </p>
+          <div>
+            <p>I'm a thing in the top right</p>
+          </div>
+        </div>
+  
+        <div className="center">
+            hello
+        </div>
+  
+        <div className="grid">
+            classname grid
+        </div>
+      </main>
+    );
+  }
+  
