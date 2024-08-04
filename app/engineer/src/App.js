ff8879484efab2346854a8fb022d974795091b06
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-08-04 13:27:58 +1200

    Implement Aref's sine graph engineer code; all prior functionality working but logic after matching sine waves unimplemented by Aref (src folder has been left in engineer folder but is useless now).

--- app/engineer/src/App.js
+++ app/engineer/src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
-import SineGraph from './SineGraph';
-
-
+import SineGraph from '../../ui/engineer/SineGraph';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <div style={{
             border: '2px solid black',  // Border thickness, style, and color
             padding: '10px',            // Optional: Add padding inside the border
             margin: '10px'              // Optional: Add margin outside the border
             }}>
             <h1>Match the waves!</h1>
-            <SineGraph/>
+            <SineGraph />
             
             <div style={{ border: 'none' }}>
                 
